d26b41054926a5a810a3416e60c286b797a106c9
neotheicebird <neotheicebird@gmail.com>
2020-02-02 23:47:59 +0530

    initial commit

--- saleor/settings.py
+++ saleor/settings.py
@@ -36,7 +36,7 @@ ROOT_URLCONF = "saleor.urls"
 WSGI_APPLICATION = "saleor.wsgi.application"
 
 ADMINS = (
-    # ('Your Name', 'your_email@example.com'),
+    ('Prashanth', 'neotheicebird@gmail.com'),
 )
 MANAGERS = ADMINS
 
